4e12af782705efe0cc8ec29085d5467991c72e6b
Nicole Fernandez Sanchez <n.fernandez17@unapec.edu.do>
2026-03-26 18:12:28 -0400

    first commit

---
+++ models.py
@@ -0,0 +1,33 @@
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from database import Base
+
+class User(Base):
+    __tablename__ = 'user'
+    
+    id_user = Column(Integer, primary_key=True)
+    patient_first_name = Column(String, nullable=False)
+    patient_last_name = Column(String, nullable=False)
+    patient_birth_date = Column(DateTime, nullable=False)
+    patient_health_condition = Column(String)
+    caregiver_name = Column(String)
+    email = Column(String, unique=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    
+class Device(Base):
+    __tablename__ = 'device'
+    
+    id_device = Column(Integer, primary_key=True)
+    serial_number = Column(String, unique=True, nullable=False)
+    api_key_hash = Column(String, nullable=False)
+    config_version = Column(Integer, nullable=False, default=0)
+    id_user = Column(Integer, ForeignKey('user.id_user', ondelete='CASCADE'), nullable=False)
+    
+class Module(Base):
+    __tablename__ = 'module'
+    
+    id_module = Column(Integer, primary_key=True)
+    pill_name = Column(String, nullable=False)
+    dose_times = Column(String, nullable=False)
+    daily_qty = Column(Integer, nullable=False)
+    id_device = Column(Integer, ForeignKey('device.id_device', ondelete='CASCADE'), nullable=False)
